b880f04dfa9901cea4ff6a606591f8c69125a23f
MORET, EDUARDO <emoret@itteria.com>
2019-09-21 21:00:33 +0200

    [fix] added ';'

--- CubeUtils/Interfaces/ICubeManager.cs
+++ CubeUtils/Interfaces/ICubeManager.cs
@@ -7,6 +7,6 @@ namespace CubeUtils.Interfaces
         float GetIntersectionVolume(Cube cube1, Cube cube2);
         bool HasCollision(Cube cube1, Cube cube2);
         float GetSubstractSpacesBetweenEdgesInAxis(float axisValueCube1, float axisValueCube2, float cube1Width, float cube2Width);
-        Point GetFirstCubeMatrixPosition(Cube cube)
+        Point GetFirstCubeMatrixPosition(Cube cube);
     }
 }
