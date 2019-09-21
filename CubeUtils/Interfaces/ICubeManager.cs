326bd7cd584de3dc7929444fec6b72d89acc938f
MORET, EDUARDO <emoret@itteria.com>
2019-09-21 20:57:32 +0200

    [fix] remove firm from interface

--- CubeUtils/Interfaces/ICubeManager.cs
+++ CubeUtils/Interfaces/ICubeManager.cs
@@ -5,7 +5,6 @@ namespace CubeUtils.Interfaces
     public interface ICubeManager
     {
         float GetIntersectionVolume(Cube cube1, Cube cube2);
-        Point GetFirstCubeMatrixPosition(Cube cube);
         bool HasCollision(Cube cube1, Cube cube2);
     }
 }
