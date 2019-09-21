c61cf7df5974cea177157f25954667f1a42e45d5
MORET, EDUARDO <emoret@itteria.com>
2019-09-21 20:59:54 +0200

    [fix] ICubManager interface, added all methods

--- CubeUtils/Interfaces/ICubeManager.cs
+++ CubeUtils/Interfaces/ICubeManager.cs
@@ -6,5 +6,7 @@ namespace CubeUtils.Interfaces
     {
         float GetIntersectionVolume(Cube cube1, Cube cube2);
         bool HasCollision(Cube cube1, Cube cube2);
+        float GetSubstractSpacesBetweenEdgesInAxis(float axisValueCube1, float axisValueCube2, float cube1Width, float cube2Width);
+        Point GetFirstCubeMatrixPosition(Cube cube)
     }
 }
