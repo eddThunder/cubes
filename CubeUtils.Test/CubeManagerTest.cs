a13039eb125d2f8656f9146a10ba1a1726e4a2c8
MORET, EDUARDO <emoret@itteria.com>
2019-09-30 09:00:34 +0200

    refactoring CubeManagerTest

--- CubeUtils.Test/CubeManagerTest.cs
+++ CubeUtils.Test/CubeManagerTest.cs
@@ -9,7 +9,7 @@ namespace CubeUtils.Test
     [TestClass]
     public class CubeManagerTest
     {
-        ICubeManager cubeManager;
+        CubeManager cubeManager { get; set; }
 
         [TestInitialize]
         public void TestInit()
@@ -20,75 +20,85 @@ namespace CubeUtils.Test
         [TestMethod]
         public void GetFirstCubeMatrixPosition_should_return_true()
         {
+            //Arrange
             var cube = new CubeBuilder().WithSize(4).WithCenterX(4).WithCenterY(4).WithCenterZ(4).Build();
-
             var point = new PointBuilder().WithX(2).WithY(2).WithZ(2).Build();
 
+            //Act
             var result = cubeManager.GetFirstCubeMatrixPosition(cube);
 
+            //Assert
             Assert.IsTrue(ComparePoints(result, point));
         }
 
         [TestMethod]
         public void HasCollision_should_return_true()
         {
+            //Arrange
             var cube1 = new CubeBuilder().WithSize(4).WithCenterX(4).WithCenterY(4).WithCenterZ(4).Build();
-
             var cube2 = new CubeBuilder().WithSize(4).WithCenterX(6).WithCenterY(6).WithCenterZ(4).Build();
 
+            //Act
             var result = cubeManager.HasCollision(cube1, cube2);
 
+            //Assert
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void HasCollision_should_return_false()
         {
+            //Arrange
             var cube1 = new CubeBuilder().WithSize(2).WithCenterX(2).WithCenterY(2).WithCenterZ(2).Build();
-
             var cube2 = new CubeBuilder().WithSize(3).WithCenterX(5).WithCenterY(4).WithCenterZ(4).Build();
 
+            //ACT
             var result = cubeManager.HasCollision(cube1, cube2);
 
+            //Assert
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void GetRestBetweenSpacesInAxis_should_return_true()
         {
+            //Arrange
             float axisValueCube1 = 2;
             float axisValueCube2 = 4;
             float cube1Side = 4;
             float cube2Side = 4;
-
             float expectedResult = 2;
 
+            //Act
             var result = cubeManager.GetSubstractSpacesBetweenEdgesInAxis(axisValueCube1, axisValueCube2, cube1Side, cube2Side);
 
+            //Assert
             Assert.AreEqual(result, expectedResult);
         }
 
         [TestMethod]
         public void GetIntersectionVolume_should_return_true()
         {
+            //Arrange
             var cube1 = new CubeBuilder().WithSize(4).WithCenterX(4).WithCenterY(4).WithCenterZ(4).Build();
-
             var cube2 = new CubeBuilder().WithSize(4).WithCenterX(6).WithCenterY(6).WithCenterZ(4).Build();
+            float expectedResult = 16;
 
+            //Act
             var result = cubeManager.GetIntersectionVolume(cube1, cube2);
 
-            float expectedResult = 16; 
-
+            //Assert
             Assert.AreEqual(result, expectedResult);
         }
 
         [TestMethod]
         public void ComparePoints_should_return_true()
         {
+            //Arrange
             var point1 = new PointBuilder().WithX(2).WithY(2).WithZ(2).Build();
-
             var point2 = new PointBuilder().WithX(2).WithY(2).WithZ(2).Build();
 
+            //Assert
             Assert.IsTrue(ComparePoints(point1, point2));
         }
 
